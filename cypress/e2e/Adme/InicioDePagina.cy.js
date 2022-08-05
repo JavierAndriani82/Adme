d1d9aa95307ac4c0cd96d3b9382cd1cd8b358339
Javier <andrianijavieranalistaqa@gmail.com>
2022-08-05 16:35:43 -0300

    ultima modificacion2

--- cypress/e2e/Adme/InicioDePagina.cy.js
+++ cypress/e2e/Adme/InicioDePagina.cy.js
@@ -9,7 +9,8 @@ describe("Primer inicio", () => {
         cy.title('eq', 'Adme - The social network that pays money')
         
         cy.get(".navbar-nav > :nth-child(1) > .page-scroll").should('be.visible').click()
-    
+        cy.wait(3000)
+        cy.get(".navbar-nav > :nth-child(2) > .page-scroll").should("be.visible").click()
     })
 
 })
