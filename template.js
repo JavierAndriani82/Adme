e1199e6e16ec808a4c0e67fee3b5ecb9158a04c3
Javier <andrianijavieranalistaqa@gmail.com>
2022-08-05 10:15:25 -0300

    Adme

---
+++ template.js
@@ -0,0 +1,16 @@
+/// <reference types="Cypress"/>
+// para que funcionen los comandos
+import 'cypress-file-upload';
+require('@4tw/cypress-drag-drop')
+require('cypress-xpath')
+require('cypress-plugin-tab')
+
+
+describe("       ", () => {
+
+    it("     ", () => {
+        cy.visit("  ")
+    
+    })
+
+})
