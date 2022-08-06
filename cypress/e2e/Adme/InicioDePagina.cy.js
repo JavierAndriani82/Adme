47ee0e652ad8be4a4eef236cffa9e7e690663bff
JavierAndriani82 <94701683+JavierAndriani82@users.noreply.github.com>
2022-08-06 00:37:39 -0300

    Update InicioDePagina.cy.js

--- cypress/e2e/Adme/InicioDePagina.cy.js
+++ cypress/e2e/Adme/InicioDePagina.cy.js
@@ -7,10 +7,14 @@ describe("Primer inicio", () => {
     it("Iniciando en Adme", () => {
         cy.visit("https://adme.com.ar")
         cy.title('eq', 'Adme - The social network that pays money')
+        let tiempo=3000
         
         cy.get(".navbar-nav > :nth-child(1) > .page-scroll").should('be.visible').click()
-        cy.wait(3000)
+        cy.wait(tiempo)
         cy.get(".navbar-nav > :nth-child(2) > .page-scroll").should("be.visible").click()
+        cy.wait(tiempo)
+        cy.get(":nth-child(3) > .page-scroll").should("be.visible").click()
+
     })
 
 })
